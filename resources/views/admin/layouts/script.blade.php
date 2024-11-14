a3124f527349bf4939af64ac510ec9d6e495b6bd
bishal <bishal@bookmundi.com>
2024-11-14 07:51:05 +0545

    Authentication

---
+++ resources/views/admin/layouts/script.blade.php
@@ -0,0 +1,15 @@
+<!-- jQuery -->
+<script src="{{url('admin/vendors/jquery/dist/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{url('admin/vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{url('admin/vendors/fastclick/lib/fastclick.js')}}"></script>
+<!-- NProgress -->
+<script src="{{url('admin/vendors/nprogress/nprogress.js')}}"></script>
+<!-- bootstrap-wysiwyg -->
+<script src="{{url('admin/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js')}}"></script>
+<script src="{{url('admin/vendors/jquery.hotkeys/jquery.hotkeys.js')}}"></script>
+<script src="{{url('admin/vendors/google-code-prettify/src/prettify.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{url('admin/vendors/js/custom.min.js')}}"></script>
---
+++ resources/views/admin/layouts/style.blade.php
@@ -0,0 +1,19 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- Meta, title, CSS, favicons, etc. -->
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Gentelella Alela! | </title>
+
+<!-- Bootstrap -->
+<link href="admin/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="admin/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- NProgress -->
+<link href="admin/vendors/nprogress/nprogress.css" rel="stylesheet">
+<!-- bootstrap-wysiwyg -->
+<link href="admin/vendors/google-code-prettify/bin/prettify.min.css" rel="stylesheet">
+
+<!-- Custom styling plus plugins -->
+<link href="admin/vendors/css/custom.min.css" rel="stylesheet">
