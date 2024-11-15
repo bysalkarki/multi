45b1eae83db8b9a8b740440594b48cf235dcfb99
bishal <bishal@bookmundi.com>
2024-11-15 09:49:15 +0545

    authentication

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'admin.index')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+
+Route::middleware(['auth'])->group(function () {
+    Route::view('dashboard', 'admin.index')
+        ->middleware(['verified'])
+        ->name('dashboard');
+
+    Route::view('profile', 'profile')
+        ->name('profile');
+
+    Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
+});
 
 require __DIR__ . '/auth.php';
